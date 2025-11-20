617da2ee8a009baa661289c48eb8c2fd4e661c9b
bijon2002 <bijosilin02@gmail.com>
2025-11-20 21:06:49 +0530

    css inline,stylesheet in react

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,21 @@
+import './header.css'
+import styles from './header.module.css'
+
+
 function Header()
 {
+
+    const myStyle={
+        color:"blue",
+        backgroundColor:"yellow",
+        padding:"10px",
+        fontFamily:"Arial",
+       
+    };
     return(
     <>
-        <h1 style={{color:"red"}}>Hello styling</h1>
+        {/* <h1 style={myStyle}>Hello styling</h1> */}
+        <h1 className={styles.bigblue}>Hello styling</h1>
         <p>add little style</p>
 
     </>
